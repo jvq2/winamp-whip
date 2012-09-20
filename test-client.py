5ed113e39b97245bcd09bdc9fd8b04896a86eba2
Joey <joey.v.q@gmail.com>
2012-09-19 23:00:54 -0400

    Initial Commit

---
+++ test-client.py
@@ -0,0 +1,26 @@
+import socket
+import sys
+
+
+HOST, PORT = "localhost", 696969
+#data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	# Connect to server
+	sock.connect((HOST, PORT))
+	while True:
+		data = input("Send: ")
+		sock.sendall(bytes(data + "\n", "utf-8"))
+		
+		# Receive data from the server
+		received = str(sock.recv(1024), "utf-8")
+		print("    Sent: {0}".format(data))
+		print("Received: {0}".format(received))
+		
+finally:
+	sock.close()
+
+input()
---
+++ winampwhip.py
@@ -0,0 +1,115 @@
+import socketserver
+import threading
+from ctypes import windll, wintypes
+import traceback
+
+SendMessageW = windll.user32.SendMessageW
+FindWindowW = windll.user32.FindWindowW
+WinampHwnd = FindWindowW("Winamp v1.x", 0)
+
+ cmdcodes = {
+	'prev':    40044,
+	'next':    40048,
+	'play':    40045,
+	'pause':   40046,
+	'stop':    40047,
+	'fadeout': 40157,
+	'forward': 40148,
+	'rewind':  40144,
+	'volup':   40058,
+	'voldown': 40059
+	}
+
+
+
+def getWinamp(wclass="Winamp v1.x"):
+	return windll.user32.FindWindowW("Winamp v1.x", 0)
+
+def play(hwnd=None):# 40045
+	global cmdcodes
+	if not hwnd: hwnd = getWinamp()
+	#if not hwnd: return -1
+	return SendMessageW(hwnd, 0x0111, cmdcodes['play'], 0)
+	
+def pause(hwnd=None):# 40046
+	global cmdcodes
+	if not hwnd: hwnd = getWinamp()
+	#if not hwnd: return -1
+	return SendMessageW(hwnd, 0x0111, cmdcodes['pause'], 0)
+	
+	
+class RemoteServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+	
+	
+class RemoteServer_handler(socketserver.StreamRequestHandler):
+	self.winampWnd = 0
+	self.authd = 0
+	
+	
+	
+	def setup(self):
+		self.winampWnd = getWinamp()
+		
+	def handle(self):
+		while not self.rfile.closed:
+			
+			try:
+				self.data = self.rfile.readline()
+			except:
+				print("{0} closed the connection".format(self.client_address[0]))
+				break
+			
+			# close if nothing is returned from the stream
+			if not self.data:
+				break # connection closed
+			
+			try:
+				self.magic()
+			except:
+				# NEVER FAIL!!
+				traceback.print_exc()
+			
+			self.wfile.write(self.data.upper())
+	
+	def magic(self):
+		self.data = self.data.strip()
+		
+		print("{0} wrote:".format(self.client_address[0]))
+		print(self.data)
+		
+		self.cmd, *self.params = self.data.split()
+		
+		return self.dispatch(self.cmd, self.params)
+	
+	
+	def dispatch(self, cmd, params=[]):
+		if not cmd: return
+		
+		if cmd == 'play':
+			return play()
+		elif cmd == 'pause':
+			return pause()
+		return
+	
+	
+	def finish(self):
+		## Note:
+		##   If setup() or handle() raise an exception, this function
+		##   will not be called.
+		pass
+	
+	
+
+if __name__ == "__main__":
+	HOST, PORT = "localhost", 696969
+
+	# Create the server, binding to localhost on port 9999
+	server = RemoteServer((HOST, PORT), RemoteServer_handler)
+
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	server.serve_forever()
+	input()
+
